2015f2a02b06c3f5ba0726a24e65b7a83700e835
klcysn <klcysn@users.noreply.github.com>
2021-01-20 12:04:18 +0100

    Add models

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Post, PostView, Comment, Like, BadPostWarning
 
+admin.site.register(Category)
+admin.site.register(PostView)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(BadPostWarning)
 # Register your models here.
